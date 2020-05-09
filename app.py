6872db5b892c08ecc6333e823ad7a4a005affd03
abdelrahman wael <abdel.rahman.wael1@gmail.com>
2020-05-09 23:17:48 +0200

    added classification interface

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask,render_template, jsonify
-import torchvision.models as models
+from flask import Flask,render_template, jsonify,request
+# import torchvision.models as models
+import numpy as np
 import torch 
+from skimage.transform import resize
+from model import MRNet
 
 app = Flask(__name__)
 
@@ -14,10 +17,41 @@ def classifcation():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    densenet = models.densenet161(pretrained=True)
-    # file_ = request.files['file']
-    print(file_)
-    return jsonify({'class_id': 'IMAGE_NET_XXX', 'class_name': 'Cat'})
+    
+    patient = request.files['file']
+    patient=load_transform_image(patient)
+    # print(patient.shape)
+    model = get_model()
+    pred = model(patient)
+    pred=pred.item()
+    print()
+    print()
+    print()
+    print(pred)
+    if(pred>0.5):
+        label = "found tear"
+    else:
+        label="no tear"
+
+    return jsonify({'pred': str(pred),"label":str(label)})
+
+
+def load_transform_image(patient):
+    
+    vol=np.load(patient).astype(np.int32)
+    vol=np.transpose(vol)
+    vol = resize(vol, (224, 224))
+    vol=np.transpose(vol)
+    vol = np.stack((vol,)*3, axis=1)
+    vol_tensor = torch.FloatTensor(vol)
+    return vol_tensor
+
+
+def get_model():
+    model=MRNet()
+    state_dict = torch.load("/home/abdel/Documents/web developlement/flusk knee MRI/model weights/f1score_val0.7155_train0.8096_epoch14", map_location=(None))
+    model.load_state_dict(state_dict)
+    return model
 
 
 @app.route("/image captioning")
